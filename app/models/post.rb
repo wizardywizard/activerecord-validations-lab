b1fca35a6359f6fbea02174c481a9acaceee3bfd
wizardywizard <littledavid175@gmail.com>
2019-11-30 17:24:55 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,10 +6,8 @@ class Post < ActiveRecord::Base
     validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
     validate :non_clickbait
     BAITS = ["Won't Believe", "Secret", "Top [number]", "Guess"]
-    def non_clickbait
-        
-        if BAITS.none? { |bait| bait.match(title)
-        binding.pry}
+    def non_clickbait        
+        if  title && BAITS.none? { |bait| title.include?(bait)}
             errors.add( :title, "Not clickbait-y")
         end
     end
