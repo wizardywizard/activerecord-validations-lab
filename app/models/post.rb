f0355962d5224c5c0aaa6714e7bcf2b79530819d
wizardywizard <littledavid175@gmail.com>
2019-11-30 17:16:08 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
+
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :non_clickbait
+    BAITS = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+    def non_clickbait
+        
+        if BAITS.none? { |bait| bait.match(title)
+        binding.pry}
+            errors.add( :title, "Not clickbait-y")
+        end
+    end
 end
